5da2ced116bfa9de844c7a8e796cd7630ec97b53
Giftedlove <138694518+Giftedlove@users.noreply.github.com>
2023-11-11 09:00:38 +0000

    Function that handles strfunc.c

--- strfunc.c
+++ strfunc.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * _puts - writes a string to standard output
- * @str: string to write
+ * _puts - the function that writes a string to standard output
+ * @str: the string to write
  *
- * Return: number of chars printed or -1 on failure
+ * Return: the number of chars printed or -1 on failure
  */
 ssize_t _puts(char *str)
 {
@@ -21,8 +21,8 @@ ssize_t _puts(char *str)
 }
 
 /**
- * _strdup - returns pointer to new mem alloc space which contains copy
- * @strtodup: string to be duplicated
+ * _strdup - the function that returns pointer to new mem alloc space which contains copy
+ * @strtodup: the string to be duplicated
  * Return: a pointer to the new duplicated string
  */
 char *_strdup(char *strtodup)
@@ -45,9 +45,9 @@ char *_strdup(char *strtodup)
 }
 
 /**
- * _strcmpr - compares two strings
- * @strcmp1: first string, of two, to be compared in length
- * @strcmp2: second string, of two, to be compared
+ * _strcmpr - the function that compares two strings
+ * @strcmp1: the first string, of two, to be compared in length
+ * @strcmp2: the second string, of two, to be compared
  * Return: 0 on success, anything else is a failure
  */
 int _strcmpr(char *strcmp1, char *strcmp2)
@@ -65,10 +65,10 @@ int _strcmpr(char *strcmp1, char *strcmp2)
 }
 
 /**
- * _strcat - concatenates two strings
- * @strc1: first string
- * @strc2: second string
- * Return: pointer
+ * _strcat - the function that concatenates two strings
+ * @strc1: the first string
+ * @strc2: the second string
+ * Return: a pointer
  */
 char *_strcat(char *strc1, char *strc2)
 {
@@ -105,9 +105,9 @@ char *_strcat(char *strc1, char *strc2)
 }
 
 /**
- * _strlen - returns the length of a string
- * @str: string to be measured
- * Return: length of string
+ * _strlen - the function that returns the length of a string
+ * @str: the string to be measured
+ * Return: the length of string
  */
 unsigned int _strlen(char *str)
 {
