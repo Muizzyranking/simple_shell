fed16a6d88e06b678b61232d30c861bcdf6dcb3b
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-11-12 13:38:10 -0500

    functions that allocates mememory

--- memory_allocation.c
+++ memory_allocation.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+#include "main.h"
 
 /**
  * _realloc - function that reallocates a pointer to double the space
@@ -9,20 +9,20 @@
  */
 char **_realloc(char **ptr, size_t *size)
 {
-	char **new;
+	char **new_mem;
 	size_t i;
 
-	new = malloc(sizeof(char *) * ((*size) + 10));
-	if (new == NULL)
+	new_mem = malloc(sizeof(char *) * ((*size) + 10));
+	if (new_mem == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 	for (i = 0; i < (*size); i++)
 	{
-		new[i] = ptr[i];
+		new_mem[i] = ptr[i];
 	}
 	*size += 10;
 	free(ptr);
-	return (new);
+	return (new_mem);
 }
